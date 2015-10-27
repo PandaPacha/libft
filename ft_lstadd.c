12c364c0891d4a3db0156d5031fcb28096a45ac4
Marc F <mafagot@student.42.fr>
2015-10-27 18:29:57 +0100

    lstadd fixed

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: mafagot <mafagot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/06 11:29:34 by mafagot           #+#    #+#             */
-/*   Updated: 2015/01/06 11:29:36 by mafagot          ###   ########.fr       */
+/*   Updated: 2015/10/27 18:26:09 by mafagot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void		ft_lstadd(t_list **alst, t_list *new)
 	if (new)
 	{
 		if (!(*alst))
-		{
-			*alst = new;
-		}
+			new->next = NULL;
 		new->next = *alst;
 		*alst = new;
 	}
